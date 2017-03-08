2385f40c046613dff187ee2510a22792b0f823ee
Ryan Au <auryan898@gmail.com>
2017-03-08 09:28:44 -0500

    motor controller tests successful

--- opmodes/NxtCtl/test.py
+++ opmodes/NxtCtl/test.py
@@ -1,18 +1,17 @@
-import inputs
-import thread
 
-#g1 = inputs.DeviceManager().gamepads[0]
-#g2 = inputs.DeviceManager().gamepads[1]
+from nxt.sensor import *
+from nxt.sensor.hitechnic import *
+import nxt, time
+import nxt.bluesock # Make sure you remember this!
+b = nxt.bluesock.BlueSock('00:16:53:10:22:3D').connect() #replace the string with the one you got earlier
+#b = nxt.find_one_brick()
 
-def update(g):
-    while 1:
-        events = g.read()
-        for event in events:
-            print(event.code,event.state)
-for g in inputs.DeviceManager().gamepads:
-    thread.start_new_thread(update,(g, ))
-#thread.start_new_thread(update,(g1, ))
-#thread.start_new_thread(update,(g2, ))
+MotorCon(b,PORT_4,0).set_power(1,10)
+MotorCon(b,PORT_4,1).set_power(1,10)
 
 while 1:
-    pass
+    time.sleep(.01)
+    print('Touch:', Touch(b, PORT_1).get_sample())
+    print('Sound:', Sound(b, PORT_2).get_sample())
+    print('Light:', Light(b, PORT_3).get_sample())
+#print('Ultrasonic:', Ultrasonic(b, PORT_4).get_sample())
