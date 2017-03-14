93c77d86d482705f7505b425debe7afc86f897a6
Ryan Au <auryan898@gmail.com>
2017-03-14 10:10:17 -0400

    added Rollie opmode and completed SoundBrick.py

--- opmodes/NxtCtl/SoundBrick.py
+++ opmodes/NxtCtl/SoundBrick.py
@@ -1,5 +1,5 @@
 import nxt
-note_str = "la la# lb lc lc# ld ld# le lf lf# lg lg# a a# b c c# d d# e f f# g g# ha ha#"
+note_str = "la la# lb lc lc# ld ld# le lf lf# lg lg# a a# b c c# d d# e f f# g g# ha ha# hb hc hc# hd hd# he hf hf# hg hg# 2a"
 note_arr = note_str.split(" ")
 note_freq = {}
 def getFreqFunc(x):
@@ -22,28 +22,104 @@ def csvToSong(filename):
     batch = []
     for row in c:
         batch.append(row)
+    print batch
     return batch
     #    print(row)
+import threading
+class PlayerObj(threading.Thread):
+    FUNC_TYPE = 0
+    ARG1 = None
+    ARG2 = None
+    ARG3 = None
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        if(self.FUNC_TYPE==1):
+            self.playSoundSample(self.brick,self.ARG1,self.ARG2)
+        if(self.FUNC_TYPE==2):
+            self.playSound(self.brick,self.ARG1,self.ARG2)
+        if(self.FUNC_TYPE==3):
+            self.playNote(self.brick,self.ARG1,self.ARG2,self.ARG3)
+        self.FUNC_TYPE = 0
+        self.ARG1 = None
+        self.ARG2 = None
+        self.ARG3 = None
 
-def playSound(b,batch,tempo):
-    #tempo is a BPM number for the song
-    #batch is a 2d list that contains a list for each note [[le,2,0],[lf#,1,0]]
-    #f = open(input('Enter file to open') + '.csv','r')
-    #c = csv.reader(f)
-    #batch = []
-    #for row in c:
-    #    batch.append(row)
-    #    print(row)
-    raw = []
-    for item in batch:
-        raw.append([note_freq[item[0]],item[1],item[2]])
-    print(raw)
-    #tempo = 120#int(input('What tempo do you want to play this as in BPM'))
-    cor_tempo = (1000/(tempo/60))/4
-    #print('Milliseconds per semi-quaver is:' + str(cor_tempo))
-    for item in raw:
-        #print(item)
-        b.play_tone_and_wait(int(item[0]), int(item[1]) * int(cor_tempo))
-        time.sleep(float(item[2]) * cor_tempo/1000)
+    def safeStart(self):
+        if not self.isAlive():
+            self.start()
+    def playSoundSample(self,b,batchNum,tempo):
+        bat = []
+        #That Rally Call tune
+        bat.append("d,1,1 d,2,0 f#,2,0 ha,3,0 f#,1,0 ha,3,0")
+        #Sad noise when mistakes are made
+        bat.append("g,1,0 f#,1,0 f,1,0 e,3,0")
+        bat.append("b,1,0 d,1,0 c,1,0 e,1,0")
+        bat.append("a,1,0 c,1,0 e,1,0 ha,1,0")
+        bat.append("a,1,0 f,1,0 e,1,0")
+        bat.append("lb,1,1 lb,2,0")
+
+        batchArr = [arr.split(",") for arr in bat[batchNum].split(" ")]
+
+        # A F# E
+        # B D C E
+        # A C E hA
+        self.playSound(b,batchArr,tempo)
+    def playSound(self,b,batch,tempo):
+        #tempo is a BPM number for the song
+        #batch is a 2d list that contains a list for each note [[le,2,0],[lf#,1,0]]
+        #f = open(input('Enter file to open') + '.csv','r')
+        #c = csv.reader(f)
+        #batch = []
+        #for row in c:
+        #    batch.append(row)
+        #    print(row)
+        raw = []
+        for item in batch:
+            raw.append([note_freq[item[0]],item[1],item[2]])
+        
+        #tempo = 120#int(input('What tempo do you want to play this as in BPM'))
+        cor_tempo = (1000/(tempo/60))/4
+        #print('Milliseconds per semi-quaver is:' + str(cor_tempo))
+        for item in raw:
+            #print(item)
+            b.play_tone_and_wait(int(item[0]), int(item[1]) * int(cor_tempo))
+            time.sleep(float(item[2]) * cor_tempo/1000)
+    def playNote(self,b,name,duration=1,variance=0):
+        #name from the note_freq dictionary, duration is in Milliseconds
+        #variance is percentage pitch modulation from given to tone to next tones
+            #ex. 0.5 to next halfstep, 1 to next wholestep, negative for lower pitch
+        freq = note_freq[name]*(2.0**(1.0/12.0))**(variance*2)
+        b.play_tone(int(freq),duration)
 
 #SoundBrick.playSound(csvToSong("victory"),120)
+
+class Player(object):
+    def __init__(self,brick):
+        self.obj = PlayerObj()
+        self.brick = brick
+    def playSoundSample(self,batchNum,tempo):
+        self.safeCreation()
+        self.obj.brick = self.brick
+        self.obj.FUNC_TYPE = 1
+        self.obj.ARG1 = batchNum
+        self.obj.ARG2 = tempo
+        self.obj.safeStart()
+    def playSound(self,batch,tempo):
+        self.safeCreation()
+        self.obj.brick = self.brick
+        self.obj.FUNC_TYPE = 2
+        self.obj.ARG1 = batch
+        self.obj.ARG2 = tempo
+        self.obj.safeStart()
+    def playNote(self,noteName,duration,variance):
+        self.safeCreation()
+        self.obj.brick = self.brick
+        self.obj.FUNC_TYPE = 3
+        self.obj.ARG1 = noteName
+        self.obj.ARG2 = duration
+        self.obj.ARG3 = variance
+        self.obj.safeStart()
+    def safeCreation(self):
+        if not self.obj.isAlive():
+            self.obj = PlayerObj()
---
+++ opmodes/pyBotRollie.py
@@ -0,0 +1,58 @@
+from NxtCtl import SoundBrick
+import nxt, thread, time
+import nxt.bluesock # Make sure you remember this!
+from NxtCtl.GamePad import *
+#b = nxt.bluesock.BlueSock('00:16:53:10:22:3D').connect() #replace the string with the one you got earlier
+b = nxt.bluesock.BlueSock('00:16:53:16:12:02').connect() #replace the string with the one you got earlier
+#b = nxt.find_one_brick()
+motorLeft = nxt.Motor(b, nxt.PORT_A)
+motorRight = nxt.Motor(b, nxt.PORT_B)
+motorCenter = nxt.Motor(b, nxt.PORT_C)
+seconds = 0
+M1POS = motorCenter.get_tacho().tacho_count
+
+
+def runToPosition(newPos):
+	oldPos = M1POS
+	diff = oldPos-newPos
+
+	if (cercaDe(motorCenter.get_tacho().tacho_count,newPos,3)):
+		motorCenter.brake()
+		diff = 0
+	motorCenter.run(Range.clip(diff*2,-90,90))
+def cercaDe(first, second, difference):
+	return abs(first-second)<=difference
+motorCenter.reset_position(True)
+
+soundPlayer = SoundBrick.Player(b)
+
+while 1:
+	print "Tick %d" % seconds
+	power = gamepad1.left_stick_y
+	direction = gamepad1.left_stick_x
+	right = power - direction
+	left = power + direction
+	motorLeft.run(Range.clip(left*100,-100,100))
+	motorRight.run(Range.clip(right*100,-100,100))
+	#motorCenter
+	print(gamepad1.left_trigger)
+	motorCenter.run(Range.clip(gamepad1.left_trigger*-200+100,-64,64))
+
+	if(gamepad1.start and gamepad1.guide):
+		motorCenter.run(0)
+		break
+
+	arr = [gamepad1.a, gamepad1.b,gamepad1.y,gamepad1.x,gamepad1.dpad_down,gamepad1.dpad_up]
+	#if(gamepad1.y):
+	#	soundPlayer.playSound(SoundBrick.csvToSong("victory"),120)
+	#if(gamepad1.x):
+	#	soundPlayer.playSoundSample(0,120)
+
+	for i in range(len(arr)):
+		if arr[i]:
+			soundPlayer.playSoundSample(i,120)
+			#noteArr = "lg a b c d e f g".split(" ")
+			#soundPlayer.playNote(noteArr[i],200,gamepad1.right_trigger)
+
+	seconds = seconds + 1
+	time.sleep(.001)
