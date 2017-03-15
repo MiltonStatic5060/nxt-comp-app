1c6f94e1b5a91dd742dc93d8ab95f6244411dd8f
Ryan Au <auryan898@gmail.com>
2017-03-15 06:29:01 -0400

    added help guides for use with raspberry pi or Debian/maybe other linuxes too 16.04

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get -y install python python-dev ipython
+sudo apt-get -y install bluetooth libbluetooth-dev
+sudo apt-get -y install python-pip
+sudo pip install inputs
+sudo pip install pygame
+sudo pip install pybluez
+sudo pip install progressbar
+pip install pybluez
+
