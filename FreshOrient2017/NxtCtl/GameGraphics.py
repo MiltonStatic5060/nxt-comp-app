5041f975a19ff56a5b89a6ed642ab0d1dd957b06
Ryan Au <auryan898@gmail.com>
2017-06-14 11:16:14 -0400

    started freshmen orientation stuff

---
+++ FreshOrient2017/NxtCtl/GameGraphics.py
@@ -0,0 +1,63 @@
+import pygame
+import thread
+import time
+
+class TextDisplay(object):
+    def __init__(self,name):
+        #takes in 1 argument of a name
+        self.arr = [name]
+        self.name = name
+    def reset(self):
+        self.arr = [self.name]
+    def addData(self,input0,input1=""):
+        if input1!="":
+            input1 = " : "+str(input1)
+        self.arr.append(str(input0)+str(input1))
+    def updater(self):
+        thread.start_new_thread(self.update,())
+    def update(self):
+        pygame.init()
+        screen = pygame.display.set_mode((50, 50))
+        pygame.display.set_caption(self.name)
+
+        boxHeight = 0
+        boxWidth = 0
+        # Event loop
+        while 1:
+            # Set size of background
+            background = pygame.Surface((boxWidth,boxHeight))
+            background = background.convert()
+            # Reset Background
+            background.fill(pygame.Color(255,255,255))
+            # Initilize the Font object
+            font = pygame.font.Font(None, int(20))
+            # Set the list of Strings to display
+            textArr = self.arr
+            # Reset window's size
+            boxHeight = 0
+            boxWidth = 0
+            for word in textArr:
+                text = font.render(word, 1, (0,0,0))
+                textpos = text.get_rect()
+                boxHeight+=textpos.height
+                if boxWidth<=textpos.width:
+                    boxWidth=textpos.width
+                textpos.centerx = textpos.width/2
+                textpos.centery = boxHeight-textpos.height/2
+                background.blit(text, textpos)
+            # Adapt screen size to the text's collective size
+            screen = pygame.display.set_mode((boxWidth, boxHeight))
+            # Blit everything to the screen
+            screen.blit(background, (0, 0))
+            pygame.display.flip()
+
+#telemetry = TextDisplay("First Display")
+#telemetry.updater()
+#
+#
+#while 1:
+#    time.sleep(.001)
+#    telemetry.reset()
+#    telemetry.addData("")
+#    telemetry.addData("left trigger",gamepad1.left_trigger)
+#    #print(gamepad1.left_trigger)
---
+++ FreshOrient2017/NxtCtl/hardwareMap.py
@@ -0,0 +1,18 @@
+import nxt, thread, time
+from nxt.sensor.hitechnic import *
+
+class DcMotor(MotorCon):
+    def __init__(self, brick, port, motorport):
+        super(DcMotorController,self).__init__(brick, port)
+        self.motorport = motorport
+    def set_power(self,power):
+        super(DcMotorController,self).set_power(motorport,power)
+    def get_power(self,power):
+        super(DcMotorController,self).get_power(motorport,power)
+
+class Servo(ServoCon):
+    def __init__(self, brick, port, servoport):
+        super(Servo,self).__init__(brick,port)
+        self.servoport = servoport
+    def set_pos(self, pos):
+        super(Servo,self).set_pos(self.servoport, pos)
